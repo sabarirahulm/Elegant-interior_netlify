edfc0dd09101ddabe74bf3f236f22bb74c86e776
sabarirahulm <sabarirahulm2001@gmail.com>
2024-05-09 16:13:51 +0530

    changesdone

--- src/Components/Pages/Bathroom.js
+++ src/Components/Pages/Bathroom.js
@@ -14,6 +14,7 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
 function Bathroom(){
+    
 
     const bathroomImages = [
         {
